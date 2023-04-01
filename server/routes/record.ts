227cfc4be4117a3b51d7f6d8cf3beb8d4ab572cb
dhruvil15 <76967484+dhruvil15@users.noreply.github.com>
2023-03-31 22:18:08 -0300

    add logic

--- server/routes/record.ts
+++ server/routes/record.ts
@@ -1,52 +1,3 @@
-// const express = require("express");
- 
-// userRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
-// const userRoutes = express.Router();
- 
-// // This will help us connect to the database
-// const dbo = require("../db/conn.ts");
- 
-// // This help convert the id from string to ObjectId for the _id.
-// const ObjectId = require("mongodb").ObjectId;
-
-
-// // This section will help you get a list of all the records.
-// userRoutes.route("/record").get(function (req, res) {
-//     let db_connect = dbo.getDb("test");
-//     db_connect
-//       .collection("users")
-//       .find({})
-//       .toArray(function (err, result) {
-//         if (err) throw err;
-//         res.json(result);
-//       });
-// });
-
-
-// // This section will help you update a record by id.
-// userRoutes.route("/update/:id").post(function (req, response) {
-//     let db_connect = dbo.getDb();
-//     let myquery = { _id: ObjectId(req.params.id) };
-//     let newvalues = {
-//         $set: {
-//             linkedin: req.body.linkedin,
-//             hobbies: req.body.hobbies,
-//             goals: req.body.goals,
-//             experience: req.body.experience
-//         },
-//     };
-//     db_connect
-//         .collection("users")
-//         .updateOne(myquery, newvalues, function (err, res) {
-//             if (err) throw err;
-//             console.log("1 document updated");
-//             response.json(res);
-//             }
-//         );
-// });
-
 const express = require("express");
  
 // recordRoutes is an instance of the express router.
@@ -97,5 +48,39 @@ userRoutes.route("/update/:id").post(function (req, response) {
       response.json(res);
     });
  });
+
+ userRoutes.route("/registration").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    name: req.body.name,
+    email: req.body.email,
+    eduName: req.body.eduName,
+    password: req.body.password,
+  };
+
+  db_connect.collection("users").findOne({email: req.body.email}).then((savedUser)=>{
+    if(savedUser){
+        return response.status(422).json({error:"user already exist."});
+    }else{
+      db_connect.collection("users").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        response.json(res);
+      });
+    }
+  });
+});
+
+ userRoutes.route("/login/:email").get(function (req, response) {
+  let db_connect = dbo.getDb();
+  db_connect.collection("users").findOne({email: req.param}).then((savedUser)=>{
+    if(savedUser){
+        return response.status(200);
+    }else{
+      return response.status(422).json({error:"user doesn't exists."});
+    }
+  })
+});
+
+
  
 module.exports = userRoutes;
